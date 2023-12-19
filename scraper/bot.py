55d666f624fde307636ed4c3c06cbfb4ba6dc642
Jimmy Wong <cy.jimmy.wong@gmail.com>
2023-12-18 22:52:54 -0800

    add output listing method

--- scraper/bot.py
+++ scraper/bot.py
@@ -18,11 +18,15 @@ import os
 
 class AutotraderMainBot:
     WEBSITE_NAME = "autotrader"
-    STARTING_PAGE = "https://www.autotrader.ca/cars/bc/?rcp=100&rcs=0&srt=9&prx=-2&prv=British%20Columbia&loc=BC&hprc=True&wcp=True&hhd=True&sts=Used-Damaged&inMarket=advancedSearch"
-    USER_AGENT =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'
-    CAR_INFO_CSS_SELECTOR = '#result-item-inner-div'
-    NEXT_PAGE_CSS_SELECTOR = 'a.last-page-link'
     RESULTS_FOLDER = 'scraped_data'
+    LISTING_FILE_NAME = 'listings.csv'
+    STARTING_PAGE = "https://www.autotrader.ca/cars/bc/?rcp=100&rcs=33800&srt=9&prx=-2&prv=British%20Columbia&loc=bc&hprc=True&wcp=True&sts=Used&inMarket=advancedSearch"
+    USER_AGENT =  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'
+    SNAPSHOT_RESULTS_SELECTOR = '#result-item-inner-div'
+    SNAPSHOT_NEXT_PAGE_SELECTOR = 'a.last-page-link'
+    LISTING_SPECS_SELECTOR = "#sl-card-body"
+    LISTING_WRAPPER_SELECTOR = "wrapper"
+    LISTING_JS_SELECTOR = './/script[@type="text/javascript"]'
     SNAPSHOT_FIELDS = {
             'price': ('.price-amount', 'text'),
             'title': ('.h2-title .result-title .title-with-trim', 'text'),
@@ -59,7 +63,7 @@ class AutotraderMainBot:
         return f"./{cls.RESULTS_FOLDER}/{cls.WEBSITE_NAME}_{current_time}.csv"
     
     @staticmethod
-    def append_to_file(filename, data):
+    def output_snapshot_csv(filename, data):
         # Check and create directory if it doesn't exist
         directory = os.path.dirname(filename)
         if not os.path.exists(directory):
@@ -74,24 +78,28 @@ class AutotraderMainBot:
                 writer.writeheader()  # Write header only once
                 
             writer.writerows(data)
+
+    @staticmethod
+    def output_listing_csv(data):
+        root_dir = os.path.dirname(os.path.abspath(__file__))
+        directory = os.path.join(root_dir, '..', AutotraderMainBot.RESULTS_FOLDER)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        output_path = os.path.join(directory, AutotraderMainBot.LISTING_FILE_NAME)
+        file_exists = os.path.isfile(output_path)
+
+        with open(output_path, 'a', newline='') as csvfile:
+            print("Writing to file")
+            fieldnames = data.keys()
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            if not file_exists:
+                writer.writeheader()
+
+            writer.writerow(data)
     
     @classmethod
     def extract_snapshot_info(cls, main_div):
-        car_info = {}
-        for key, (selector, attribute, *extras) in cls.SNAPSHOT_FIELDS.items():
-            try:
-                element = main_div.find_element(By.CSS_SELECTOR, selector)
-                value = element.text if attribute == 'text' else element.get_attribute(attribute)
-                if extras and extras[0] == 'strip':
-                    value = value.strip()
-                car_info[key] = value
-            except:
-                car_info[key] = None
-                
-        return car_info
-    
-    @classmethod
-    def extract_listing_info(cls, main_div):
         car_info = {}
         for key, (selector, attribute, *extras) in cls.SNAPSHOT_FIELDS.items():
             try:
@@ -112,28 +120,28 @@ class AutotraderMainBot:
 
         filename = self.create_filename()
         
-        page = 1
+        page = 0
         while True:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.CAR_INFO_CSS_SELECTOR))
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.SNAPSHOT_RESULTS_SELECTOR))
             )
 
-            main_divs = self.driver.find_elements(By.CSS_SELECTOR, self.CAR_INFO_CSS_SELECTOR)
+            main_divs = self.driver.find_elements(By.CSS_SELECTOR, self.SNAPSHOT_RESULTS_SELECTOR)
 
             page_car_info = []
             for main_div in main_divs:
                 page_car_info.append(self.extract_snapshot_info(main_div))
 
-            self.append_to_file(filename, page_car_info)
+            self.output_snapshot_csv(filename, page_car_info)
             
             if pages and page >= pages:
                 break
 
             try:
                 WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, self.NEXT_PAGE_CSS_SELECTOR))
+                    EC.presence_of_element_located((By.CSS_SELECTOR, self.SNAPSHOT_NEXT_PAGE_SELECTOR))
                 )
-                next_link = self.driver.find_element(By.CSS_SELECTOR, self.NEXT_PAGE_CSS_SELECTOR)
+                next_link = self.driver.find_element(By.CSS_SELECTOR, self.SNAPSHOT_NEXT_PAGE_SELECTOR)
                 self.driver.execute_script("arguments[0].scrollIntoView();", next_link)
                 print("Clicking next link")
                 page += 1
@@ -155,16 +163,16 @@ class AutotraderMainBot:
         page = 1
         while True:
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.CAR_INFO_CSS_SELECTOR))
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, self.SNAPSHOT_RESULTS_SELECTOR))
             )
 
-            main_divs = self.driver.find_elements(By.CSS_SELECTOR, self.CAR_INFO_CSS_SELECTOR)
+            main_divs = self.driver.find_elements(By.CSS_SELECTOR, self.SNAPSHOT_RESULTS_SELECTOR)
 
             page_car_info = []
             for main_div in main_divs:
                 page_car_info.append(self.extract_snapshot_info(main_div))
 
-            self.driver.get(page_car_info[0]['listing_url'])
+            # self.driver.get(page_car_info[0]['listing_url'])
             
             with open('sample_listing_source.html', 'w') as f:
                 f.write(self.driver.page_source)
@@ -192,11 +200,12 @@ class AutotraderMainBot:
 
         self.driver.quit()
 
-    def test_listing(self):
-        listing_page = 'https://www.autotrader.ca/a/ford/f-150/kelowna/british%20columbia/19_12734864_/?showcpo=ShowCpo&ncse=no&ursrc=pl&urp=3&urm=8&sprx=-2'
-        css_selector = '#specificationWidget'
+    def scrape_listing(self, url=None):
+        if not url:
+            url = "https://www.autotrader.ca/a/toyota/tundra/vernon/british%20columbia/19_12736412_/?showcpo=ShowCpo&ncse=no&ursrc=pl&urp=5&urm=8&sprx=-2"
+        css_selector = '#sl-card-body'
         id_selector = 'wrapper'
-        self.driver.get(listing_page)
+        self.driver.get(url)
         while True:
             WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, css_selector))
@@ -205,9 +214,7 @@ class AutotraderMainBot:
             specs_div = self.driver.find_element(By.CSS_SELECTOR, css_selector)
             specs_div_content = specs_div.get_attribute('outerHTML')
 
-            soup = BeautifulSoup(specs_div_content, 'html.parser')
-            specs_list = soup.find('ul', {'id': 'sl-card-body'})
-            
+            specs_list = BeautifulSoup(specs_div_content, 'html.parser')
             specs_dict = {}
 
             for index, li in enumerate(specs_list.find_all('li', {'class': 'list-item'})):
@@ -219,12 +226,8 @@ class AutotraderMainBot:
             script_content = script.get_attribute('innerHTML')
             vin_match = re.search(r'"vin":"([^"]*)"', script_content)
             specs_dict["VIN"] = vin_match.group(1) if vin_match else None
-            
-            for key, value in specs_dict.items():
-                print(f'{key}: {value}')
-            
-            # with open('sample_listing_source.html', 'w') as f:
-            #     f.write(self.driver.page_source)
+
+            self.output_listing_csv(specs_dict)
             
             break
         self.driver.quit()
@@ -233,7 +236,7 @@ class AutotraderMainBot:
 def main():
     bot = AutotraderMainBot()
     # bot.run(pages = 2)
-    bot.test_listing()
+    bot.scrape_listing()
 
 
 if __name__ == "__main__":
