56c69b192a65a2e58e5d4d9b45cf37d7502324fb
Jimmy Wong <cy.jimmy.wong@gmail.com>
2023-12-20 00:50:29 -0800

    check url exist instead of getting all url from db

--- scraper/database.py
+++ scraper/database.py
@@ -19,17 +19,17 @@ class Database:
   def insert_listing_details(self, data):
     self.supabase.table(self.LISTING_TABLE_NAME).insert(data).execute()
 
-  def get_all_listing_urls(self):
-    result = self.supabase.table(self.LISTING_TABLE_NAME).select("url").execute()
-    urls = [item["url"] for item in result.data]
-    return urls
+  def check_url_exist(self, url):
+    result = self.supabase.table(self.LISTING_TABLE_NAME).select("*").eq("url", url).execute()
+    return len(result.data) > 0
   
   def update_listing_price(self, url, price):
     self.supabase.table(self.LISTING_TABLE_NAME).update({"lowest_price": price}).eq("url", url).execute()
   
 def main():
   db = Database()
-  db.update_listing_price("https://www.autotrader.ca/a/acura/ilx/surrey/british%20columbia/19_12745170", 1000)
+  # db.update_listing_price("https://www.autotrader.ca/a/acura/ilx/surrey/british%20columbia/19_12745170", 1000)
+  print(db.check_url_exist("https://www.autotrader.ca/a/acura/ilx/surrey/british%20columbia/19_12745170"))
 
 if __name__ == "__main__":
   main()
